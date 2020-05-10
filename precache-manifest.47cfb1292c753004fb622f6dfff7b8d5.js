63ad6b70d81cefff4cbfdd953f2b2c186490867b
Denzel <denzeltlee@gmail.com>
2020-05-10 23:19:35 +0800

    Updates

---
+++ precache-manifest.47cfb1292c753004fb622f6dfff7b8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349ab074a9f56f6ac23f4c390e61c320",
+    "url": "/practice-react-app/index.html"
+  },
+  {
+    "revision": "01a018633adea5a3b783",
+    "url": "/practice-react-app/static/css/main.069eeb47.chunk.css"
+  },
+  {
+    "revision": "4b4c7f4b783978b6c07a",
+    "url": "/practice-react-app/static/js/2.7441ff16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/practice-react-app/static/js/2.7441ff16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a018633adea5a3b783",
+    "url": "/practice-react-app/static/js/main.8e266849.chunk.js"
+  },
+  {
+    "revision": "2a2b4a6ad981b1e548d5",
+    "url": "/practice-react-app/static/js/runtime-main.7675aa42.js"
+  }
+]);
